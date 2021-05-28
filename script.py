a64ee11e60e2e906c5d9d9bce2c7cde6074fd1ed
Nikolay Strom <kolja87@tut.by>
2021-05-28 15:25:31 +0300

    added models, filled DB

---
+++ script.py
@@ -0,0 +1,49 @@
+import os
+
+import django
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'jumanji.settings'
+django.setup()
+
+from vacancies.models import Company, Specialty, Vacancy
+from data import jobs, companies, specialties
+
+
+
+def add_entries(collections):
+    if collections is companies:
+        for collection in collections:
+            Company.objects.create(
+                name = collection['title'],
+                location = collection['location'],
+                logo = collection['logo'],
+                description = collection['description'],
+                employee_count = collection['employee_count']
+            )
+
+    elif collections is jobs:
+        for collection in collections:
+            Vacancy.objects.create(
+                title = collection['title'],
+                specialty = Specialty.objects.get(code=collection['specialty']),
+                company = Company.objects.get(id=collection['company']),
+                skills = collection['skills'],
+                description = collection['description'],
+                salary_min = collection['salary_from'],
+                salary_max = collection['salary_to'],
+                published_at = collection['posted']
+            )
+
+    elif collections is specialties:
+        for collection in collections:
+            Specialty.objects.create(
+                code=collection['code'],
+                title=collection['title'],
+            )
+
+
+
+if __name__ == '__main__':
+    add_entries(jobs)
+    add_entries(companies)
+    add_entries(specialties)
--- vacancies/models.py
+++ vacancies/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=16)
+    location = models.CharField(max_length=32)
+    logo = models.CharField(max_length=32)
+    description = models.TextField()
+    employee_count = models.IntegerField()
+
+class Specialty(models.Model):
+    code = models.CharField(max_length=16)
+    title = models.CharField(max_length=32)
+    picture = models.URLField(default='https://place-hold.it/100x60')
+
+
+class Vacancy(models.Model):
+
+    title = models.CharField(max_length=64)
+    specialty = models.ForeignKey(Specialty, related_name='vacancies', on_delete=models.CASCADE, null=True)
+    company = models.ForeignKey(Company, related_name='vacancies', on_delete=models.CASCADE, null=True)
+    skills = models.CharField(max_length=100)
+    description = models.TextField()
+    salary_min = models.IntegerField()
+    salary_max = models.IntegerField()
+    published_at = models.DateField()
+
+    def __str__(self):
+        return f'Vacancy {self.pk}'
